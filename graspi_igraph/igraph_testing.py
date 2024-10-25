2323e36b9f61a453bc5f7f3caf3d691f225c58c9
Wenqi Zheng <wenqizheng326@gmail.com>
2024-10-25 03:14:45 -0400

    fixed graph generation + visualization

--- graspi_igraph/igraph_testing.py
+++ graspi_igraph/igraph_testing.py
@@ -7,171 +7,62 @@ import sys
 
 '''---------Function to create edges for graph in specified format --------'''
 def graphe_adjList(filename):
-    adjacency_list = {}
+    adjacency_list = []
     with open(filename, "r") as file:
         header = file.readline().split()
         vertex_count = int(header[0])
-        # print(vertex_count)
         for i in range(vertex_count):
             header = file.readline().split()
-            vertex_count = header
-            # print(vertex_count)
-            j = 2
-            current_node = header[0]
             neighbors = []
-            while j in range(len(header)):
-                neighbor_node = header[j]
-                # print(current_node)
-                neighbors.append(neighbor_node)
-                j += 3
-            adjacency_list[current_node] = neighbors
-        for i in range(2):
-            header = file.readline().split()
-            vertex_count = header
-            # print(vertex_count)
-            j = 2
-            current_node = header[0]
-            neighbors = []
-            while j < len(header):
-                neighbor_node = header[j]
-                # print(current_node)
-                neighbors.append(neighbor_node)
-                j += 3
-            adjacency_list[current_node] = neighbors
+            for j in range(2,len(header),3):
+                if int(header[j]) < len(adjacency_list):
+                    if i not in adjacency_list[int(header[j])]:
+                        neighbors.append(int(header[j]))
+                else:
+                    neighbors.append(int(header[j]))
+            adjacency_list.append(neighbors)
+
+    adjacency_list.append([])
+    adjacency_list.append([])
+    
     return adjacency_list
-
-
-
-def adjList(fileName):
-    adjacency_list = {}
-    dimX = dimY = dimZ = 0
-
-    with open(fileName, "r") as file:
-        header = file.readline().split(' ')
-        dimX, dimY, dimZ = int(header[0]), int(header[1]), int(header[2])
-
-        offsets = [(-1, -1, 0), (-1, 0, 0), (0, -1, 0), (0, 0, -1), (1, -1, 0)]
-
-        for z in range(dimZ):
-            for y in range(dimY):
-                for x in range(dimX):
-                    current_vertex = x * dimY * dimZ + y * dimZ + z
-                    neighbors = []
-
-                    for dx, dy, dz in offsets:
-                        nx, ny, nz = x + dx, y + dy, z + dz
-                        if 0 <= nx < dimX and 0 <= ny < dimY and 0 <= nz < dimZ:
-                            neighbor_vertex = nx * dimY * dimZ + ny * dimZ + nz
-                            neighbors.append(neighbor_vertex)
-
-                    adjacency_list[current_vertex] = neighbors
-
-    adjacency_list[dimZ * dimY * dimX] = list(range(dimX))
-    adjacency_list[dimZ * dimY * dimX + 1] = [i + dimX * (dimY - 1) for i in range(dimX)]
-    # green_vertex = adjacency_list[dimZ * dimY * dimX + 2]
-
-
-    return adjacency_list
-
 
 '''------- Labeling the color of the vertices -------'''
-
-
-def vertexColors(fileName):
-    labels = []
-    with open(fileName, 'r') as file:
-        lines = file.readlines()
-        for line in lines[1:]:
-            for char in line:
-                if char == '1':
-                    labels.append('white')
-                elif char == '0':
-                    labels.append('black')
-
-    return labels
-
 def graphe_vertexColors(fileName):
     labels = []
     with open(fileName, 'r') as file:
         line = file.readline().split()
         vertex_count = int(line[0])
-        for i in range(vertex_count):
+        for i in range(vertex_count+2):
             line = file.readline().split()
             char = line[1]
             if char == '1':
                 labels.append('white')
             elif char == '0':
                 labels.append('black')
-
-        # lines = file.readlines()
-        # for line in lines[1:]:
-        #     for char in line[1]:
-        #         if char == '1':
-        #             labels.append('white')
-        #         elif char == '0':
-        #             labels.append('black')
+            elif char == '10':
+                labels.append('blue')
+            elif char == '20':
+                labels.append('red')
 
 
     return labels
 
 '''********* Constructing the Graph **********'''
 
-def generateGraphAdj(file):
-    edges = adjList(file)
-    labels = vertexColors(file)
-
-    f = open(file, 'r')
-    line = f.readline()
-    line = line.split()
-
-    g = ig.Graph.ListDict(edges=edges, directed=False)
-    g.vs["color"] = labels
-    g.vs[int(line[0]) * int(line[1])]['color'] = 'blue'
-    g.vs[int(line[0]) * int(line[1]) + 1]['color'] = 'red'
-
-    g.add_vertices(1)
-    g.vs[int(line[0]) * int(line[1]) + 2]['color'] = 'green'
-    green_vertex = g.vs[g.vcount() - 1]
-    for i in range(g.ecount()):
-        current_edge = g.es[i]
-        source_vertex = current_edge.source
-        target_vertex = current_edge.target
-        if(g.vs[source_vertex]['color'] == 'black' and g.vs[target_vertex]['color'] == 'white'):
-            '''connect both source and target to green meta vertex'''
-            g.add_edge(green_vertex, source_vertex)
-            g.add_edge(green_vertex, target_vertex)
-    return g
-
 def graphe_generateGraphAdj(file):
-    edges = graphe_adjList(file)
-    labels = graphe_vertexColors(file)
+    adjacency_list = graphe_adjList(file)
+    vertex_colors = graphe_vertexColors(file)
 
-    f = open(file, 'r')
-    line = f.readline()
-    line = line.split()
+    edges = [(i, neighbor) for i, neighbors in enumerate(adjacency_list) for neighbor in neighbors]
+    
+    g = ig.Graph(edges, directed=False)
+    g.vs["color"] = vertex_colors
 
-    g = ig.Graph.ListDict(edges=edges, directed=False)
-    g.vs["color"] = labels
-    print(len(edges))
-    g.vs[len(edges) - 2]['color'] = 'blue'
-    g.vs[len(edges) - 1]['color'] = 'red'
-
-    g.add_vertices(1)
-    g.vs[len(edges)]['color'] = 'green'
-    green_vertex = g.vs[g.vcount() - 1]
-    for i in range(g.ecount()):
-        current_edge = g.es[i]
-        source_vertex = current_edge.source
-        target_vertex = current_edge.target
-        if(g.vs[source_vertex]['color'] == 'black' and g.vs[target_vertex]['color'] == 'white'):
-            '''connect both source and target to green meta vertex'''
-            g.add_edge(green_vertex, source_vertex)
-            g.add_edge(green_vertex, target_vertex)
     return g
-'''Add green interface node'''
 
 def visual2D(g):
-    layout = g.layout('reingold_tilford')
+    layout = g.layout('fr')
     # fig, ax = plt.subplots()
     # ax.invert_yaxis() # reverse starting point of graph (vertex 0)
     fig, ax = plt.subplots(figsize=(10, 10))
@@ -264,7 +155,7 @@ def shortest_path(graph):
 
     return listOfShortestPaths
 
-
+"""
 def main():
     is_2D = True
     correct_input = False
@@ -316,3 +207,9 @@ def test():
 if __name__ == '__main__':
     # main()
     test()
+"""
+
+g = graphe_generateGraphAdj("tests/2D-testFile/data_4_3.graphe")
+visual2D(g)
+
+
