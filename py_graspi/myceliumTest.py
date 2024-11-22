bc171ff00e67f3cafbf0633b0e8e7b9484a500f7
kkevinmartinezz <kevin425.m@hotmail.com>
2024-11-22 13:49:20 -0500

    Task #136 - As a Researcher I want to be able to filter out and visualize only white vertices of a map so that I can visualize more filtered versions of our graph. - filtered out black vertices and outputting visualization for them as well.

--- py_graspi/myceliumTest.py
+++ py_graspi/myceliumTest.py
@@ -8,22 +8,23 @@ import img_to_txt as translate
 
 
 def visualize(g):
-    layout = g.layout('drl') #drl, kk
+    layout = g.layout('kk') #drl, kk
 
     fig, ax = plt.subplots(figsize=(10, 10))
 
     igraph.plot(g,
                 target=ax,
                 layout=layout,
+                vertex_colors=vertexColors,
                 vertex_size=5,
-                margin=5)
+                margin=20)
 
     # Remove the code that generates the labels for each vertex
     # plt.title("Visualization of White Vertices of Graph")
     plt.show()
 def filter_black_vertices(graph):
     """
-        Filters the graph by keeping only edges between vertices of the same color.
+        Filters the graph by keeping only edges between vertices of the same color {black}.
 
         Args:
             graph (ig.Graph): The input graph.
@@ -48,7 +49,7 @@ def filter_black_vertices(graph):
 
 def filter_white_vertices(graph):
     """
-        Filters the graph by keeping only edges between vertices of the same color.
+        Filters the graph by keeping only edges between vertices of the same color {white}.
 
         Args:
             graph (ig.Graph): The input graph.
@@ -80,9 +81,9 @@ def main():
     (g, is_2D, black_vertices, white_vertices, black_green, black_interface_red, white_interface_blue,
      dim, interface_edge_comp_paths, shortest_path_to_red, shortest_path_to_blue,
      CT_n_D_adj_An, CT_n_A_adj_Ca) = ig.generateGraphAdj(txt_filename)
-    print("graph created")
+    # print("graph created")
     whiteFilteredGraph = filter_white_vertices(g)
-    print("graph filtered")
+    # print("graph filtered")
     visualize(whiteFilteredGraph)
     blackFilteredGraph = filter_black_vertices(g)
     visualize(blackFilteredGraph)
