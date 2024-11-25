7e1d14a0b9c68d4ad91406c35599634e3a597654
kkevinmartinezz <kevin425.m@hotmail.com>
2024-11-25 02:02:30 -0500

    Task #136 - As a Researcher I want to be able to filter out and visualize only white vertices of a map so that I can visualize more filtered versions of our graph. - Updated myceliumTest.py so that graphs visualization is interactive. Added lambda event handler buttons that when clicked will either zoom in/out or rotate the graph

--- py_graspi/myceliumTest.py
+++ py_graspi/myceliumTest.py
@@ -1,25 +1,40 @@
 import sys
-
 import igraph
-import matplotlib.pyplot as plt
 import igraph_testing as ig
 import img_to_txt as translate
+import matplotlib.pyplot as plt
+from matplotlib.widgets import Button
+import numpy as np
 
 
 def visualize(g):
-    layout = g.layout('kk') #drl, kk
-
+    layout = g.layout('kk')  # Kamada-Kawai layout for better spacing
     fig, ax = plt.subplots(figsize=(10, 10))
+    plot = igraph.plot(g,
+                       target=ax,
+                       layout=layout,
+                       vertex_colors = g.vs["color"],
+                       vertex_size=10,
+                       margin=20)
+    axcolor = 'lightgoldenrodyellow'
 
-    igraph.plot(g,
-                target=ax,
-                layout=layout,
-                vertex_size=5,
-                margin=20)
+    ax_zoom_in = plt.axes([0.7, 0.05, 0.1, 0.075], facecolor=axcolor)
+    ax_zoom_out = plt.axes([0.81, 0.05, 0.1, 0.075], facecolor=axcolor)
+    ax_rotate = plt.axes([0.92, 0.05, 0.1, 0.075], facecolor=axcolor)
 
-    # Remove the code that generates the labels for each vertex
-    # plt.title("Visualization of White Vertices of Graph")
+    button_zoom_in = Button(ax_zoom_in, label='Zoom In')
+    button_zoom_out = Button(ax_zoom_out, label='Zoom Out')
+    button_rotate = Button(ax_rotate, label='Rotate')
+
+    current_angle = [0]
+
+    button_zoom_in.on_clicked(lambda event: zoom_in(event, ax))
+    button_zoom_out.on_clicked(lambda event: zoom_out(event, ax))
+    button_rotate.on_clicked(lambda event: rotate(event, ax, g, layout,current_angle, 30))
+
     plt.show()
+
+
 def filter_black_vertices(graph):
     """
         Filters the graph by keeping only edges between vertices of the same color {black}.
@@ -39,7 +54,10 @@ def filter_black_vertices(graph):
         toNode = edge[1]
         if (graph.vs[currentNode]['color'] == 'black') and (graph.vs[toNode]['color'] == 'black'):
             keptEdges.append(edge)
-
+        if ((graph.vs[currentNode]['color'] == 'blue') or (graph.vs[toNode]['color'] == 'blue')):
+            keptEdges.append(edge)
+        elif ((graph.vs[currentNode]['color'] == 'red') or (graph.vs[toNode]['color'] == 'red')) :
+            keptEdges.append(edge)
     filteredGraph = graph.subgraph_edges(keptEdges, delete_vertices=True)
 
     return filteredGraph
@@ -64,10 +82,95 @@ def filter_white_vertices(graph):
         toNode = edge[1]
         if (graph.vs[currentNode]['color'] == 'white') and (graph.vs[toNode]['color'] == 'white'):
             keptEdges.append(edge)
+        if ((graph.vs[currentNode]['color'] == 'blue') or (graph.vs[toNode]['color'] == 'blue')):
+            keptEdges.append(edge)
+        elif ((graph.vs[currentNode]['color'] == 'red') or (graph.vs[toNode]['color'] == 'red')) :
+            keptEdges.append(edge)
+
 
     filteredGraph = graph.subgraph_edges(keptEdges, delete_vertices=True)
 
     return filteredGraph
+
+
+def zoom_in(event, ax):
+    xlims = ax.get_xlim()
+    ylims = ax.get_ylim()
+
+    # Calculate the center of the plot
+    x_center = (xlims[0] + xlims[1]) / 2
+    y_center = (ylims[0] + ylims[1]) / 2
+
+    # Define a more aggressive zoom factor
+    factor = 0.5  # Change this value to zoom in more or less
+
+    # Calculate new limits
+    x_range = (xlims[1] - xlims[0]) * factor
+    y_range = (ylims[1] - ylims[0]) * factor
+    new_xlims = [x_center - x_range / 2, x_center + x_range / 2]
+    new_ylims = [y_center - y_range / 2, y_center + y_range / 2]
+
+    # Set new limits
+    ax.set_xlim(new_xlims)
+    ax.set_ylim(new_ylims)
+    plt.draw()
+
+
+def zoom_out(event, ax):
+    xlims = ax.get_xlim()
+    ylims = ax.get_ylim()
+
+    # Calculate the center of the plot
+    x_center = (xlims[0] + xlims[1]) / 2
+    y_center = (ylims[0] + ylims[1]) / 2
+
+    # Define a more aggressive zoom factor
+    factor = 2  # Change this value to zoom in more or less
+
+    # Calculate new limits
+    x_range = (xlims[1] - xlims[0]) * factor
+    y_range = (ylims[1] - ylims[0]) * factor
+    new_xlims = [x_center - x_range / 2, x_center + x_range / 2]
+    new_ylims = [y_center - y_range / 2, y_center + y_range / 2]
+
+    # Set new limits
+    ax.set_xlim(new_xlims)
+    ax.set_ylim(new_ylims)
+    plt.draw()
+
+
+def rotate(event, ax, g, layout,current_angle, angle):
+    # Preserve current zoom limits
+    xlims = ax.get_xlim()
+    ylims = ax.get_ylim()
+    # Convert the angle to radians
+    current_angle[0] += angle
+    angle_rad = np.deg2rad(current_angle[0])
+
+    # Get the original layout coordinates
+    coords = np.array(layout.coords)
+
+    # Rotate the coordinates
+    cos_angle = np.cos(angle_rad)
+    sin_angle = np.sin(angle_rad)
+    rotation_matrix = np.array([[cos_angle, -sin_angle], [sin_angle, cos_angle]])
+    rotated_coords = coords.dot(rotation_matrix)
+
+    # Update the layout with the rotated coordinates
+    new_layout = igraph.Layout(rotated_coords.tolist())
+    # Clear the current plot and redraw the graph with the new layout
+    ax.clear()
+    plot = igraph.plot(g,
+                       target=ax,
+                       layout=new_layout,
+                       vertex_colors=g.vs["color"],
+                       vertex_size=5,
+                       margin=20)
+
+    # Reapply the previous zoom limits
+    ax.set_xlim(xlims)
+    ax.set_ylim(ylims)
+    plt.draw()
 
 
 def main():
