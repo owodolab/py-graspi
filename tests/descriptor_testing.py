c5e3caf29a905d4f3fdf29e820dbbf2639b258ab
Julia Joseph <113621027+123julianna@users.noreply.github.com>
2025-03-28 23:30:40 -0400

    descriptor.py and myceliumTest.py work

--- tests/descriptor_testing.py
+++ tests/descriptor_testing.py
@@ -2,7 +2,7 @@ import os
 import sys
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../src")))
-import src.igraph as ig
+import src.graph as ig
 import src.descriptors as d
 
 
