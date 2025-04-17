9d99d0b4db2caa4ff60d4b325b38a8cabe78c1b6
kkevinmartinezz <kevin425.m@hotmail.com>
2025-04-17 17:29:11 -0400

    Card #94 - Added _All_ variable in init.py, also modified testingRepo.py to test for visualization, currently failing right now. Seems there is an error reaching into graph_data_class variables

--- debugging/testingRepo.py
+++ debugging/testingRepo.py
@@ -1,11 +1,19 @@
 import py_graspi
+from py_graspi import visualize
+from py_graspi.graph_data_class import graph_data_class
+# from tests.descriptor_testing import graph_data
 
 filename = "../debugging/debugging-tests/data_4_3.txt"
-graph_data = py_graspi.generateGraph(filename)
+graph_data = py_graspi.generateGraphAdj(filename)
+g = graph_data_class(graph_data, True)
+
 dict_descriptors = py_graspi.descriptors(graph_data, filename)
 py_graspi.descriptorsToTxt(dict_descriptors, "debugging-tests/output_descriptors.txt")
+py_graspi.visualize(g.graph, True)
 
 #should add following read function into descriptors.py
 with open('debugging-tests/output_descriptors.txt', 'r') as file:
     content = file.read()
     print(content)
+
+# print(type(g.is_2D))
