481eab28927ca6a43d3236cac9727b8a88834997
kkevinmartinezz <kevin425.m@hotmail.com>
2025-04-17 18:31:53 -0400

    Card #94 - Updated testingRepo.py, if this works then the package has been updated successfully, currently failing in visualizing, seemes to be passing in creating graph and outputting descriptors.

--- debugging/testingRepo.py
+++ debugging/testingRepo.py
@@ -3,13 +3,19 @@ from py_graspi import visualize
 from py_graspi.graph_data_class import graph_data_class
 # from tests.descriptor_testing import graph_data
 
-filename = "../debugging/debugging-tests/data_4_3.txt"
-graph_data = py_graspi.generateGraphAdj(filename)
+filename = "../data/data/data_0.5_2.2_001900.txt"
+graph_data = py_graspi.generateGraph(filename)
 g = graph_data_class(graph_data, True)
+print(g)
+print(type(g.is_2D))
+if True == g.is_2D:
+    print("This is what we want")
+# exit()
 
+py_graspi.shortest_path_descriptors(g.graph, filename, g.black_interface_red, g.white_interface_blue)
 dict_descriptors = py_graspi.descriptors(graph_data, filename)
 py_graspi.descriptorsToTxt(dict_descriptors, "debugging-tests/output_descriptors.txt")
-py_graspi.visualize(g.graph, True)
+py_graspi.visualize(g.graph, g.is_2D)
 
 #should add following read function into descriptors.py
 with open('debugging-tests/output_descriptors.txt', 'r') as file:
