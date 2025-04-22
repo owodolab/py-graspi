a1a8d9ac35ace276639ee03d665fb27ffa530470
Julia Joseph <113621027+123julianna@users.noreply.github.com>
2025-04-22 19:09:56 -0400

    removed readme as pypi source, manual description

--- setup.py
+++ setup.py
@@ -2,14 +2,28 @@ from setuptools import setup, find_packages
 
 setup(
     name="py_graspi",
-    author="Wenqi Zheng",
-    author_email="wenqizhe@buffalo.edu",
+    author="Olga Wodo",
+    author_email="olgawodo@buffalo.edu",
     version="0.1.1.9",
-    description="Graph-based descriptor extraction tool for microstructures",
-    long_description=open("README.md", encoding="utf-8").read(),
-    long_description_content_type="text/markdown",
-    packages=find_packages(),
-    classifiers=["Programming Language :: Python"],
+    description="Graph-based descriptor for microstructure featurization",
+    long_description="""
+    Py-GraSPI (Graph-based Structure Property Identifier) is a Python package designed to compute a comprehensive set of descriptors for segmented microstructures using a graph-based approach.
+
+    See https://github.com/owodolab/py-graspi for the source code and https://owodolab.github.io/py-graspi/ for documentation.
+    """,
+    long_description_content_type="text/plain",
+    license="BSD 3-Clause License",
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+    classifiers=["Programming Language :: Python :: 3",
+                 "License :: OSI Approved :: BSD License",
+                 "Operating System :: OS Independent"
+                 ],
+
+    url="https://github.com/owodolab/py-graspi",
+    download_url='https://github.com/owodolab/py-graspi/archive/refs/tags/v_2.0.4.tar.gz',
+    # need to get this link from the GitHub repo "Releases" section
+
     install_requires=[
         "igraph",
         "matplotlib",
