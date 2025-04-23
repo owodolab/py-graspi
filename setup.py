116f23c605b9a1f6406c209dc7242dea7c9a44ce
Julia Joseph <113621027+123julianna@users.noreply.github.com>
2025-04-23 14:08:22 -0400

    text on pypi not textbox anymore

--- setup.py
+++ setup.py
@@ -1,36 +1,36 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="py_graspi",
-    author="Olga Wodo",
-    author_email="olgawodo@buffalo.edu",
-    version="0.2.0.5",
-    description="Graph-based descriptor for microstructure featurization",
-    long_description="""
-    # Py-GraSPI (Graph-based Structure Property Identifier)
+    name = "py_graspi",
+    author = "Olga Wodo",
+    author_email = "olgawodo@buffalo.edu",
+    version = "0.2.0.6",
+    description = "Graph-based descriptor for microstructure featurization",
+    long_description = """\
+# Py-GraSPI (Graph-based Structure Property Identifier)
 
-    **Py-GraSPI** is a Python package designed to compute a comprehensive set of descriptors for segmented microstructures using a graph-based approach.
+**Py-GraSPI** is a Python package designed to compute a comprehensive set of descriptors for segmented microstructures using a graph-based approach.
 
-    ---
+---
 
-    ### Resources
+### Resources
 
-    - **Source Code**: [GitHub Repository](https://github.com/owodolab/py-graspi)
-    - **Documentation**: [Project Documentation](https://owodolab.github.io/py-graspi/)
-    """,
-    long_description_content_type="text/markdown",
-    license="BSD 3-Clause License",
-    packages=find_packages(where='src'),
-    package_dir={'': 'src'},
-    classifiers=["Programming Language :: Python :: 3",
+- **Source Code**: [GitHub Repository](https://github.com/owodolab/py-graspi)
+- **Documentation**: [Project Documentation](https://owodolab.github.io/py-graspi/)
+""",
+    long_description_content_type = "text/markdown",
+    license = "BSD 3-Clause License",
+    packages = find_packages(where='src'),
+    package_dir = {'': 'src'},
+    classifiers = ["Programming Language :: Python :: 3",
                  "License :: OSI Approved :: BSD License",
                  "Operating System :: OS Independent"
                  ],
 
-    url="https://github.com/owodolab/py-graspi",
-    download_url='https://github.com/owodolab/py-graspi/archive/refs/tags/v_2.0.4.tar.gz',
+    url = "https://github.com/owodolab/py-graspi",
+    download_url = 'https://github.com/owodolab/py-graspi/archive/refs/tags/v_2.0.4.tar.gz',
     # need to get this link from the GitHub repo "Releases" section
-    install_requires=[
+    install_requires = [
         "igraph",
         "matplotlib",
         "numpy",
@@ -47,6 +47,6 @@ setup(
         "texttable",
         "fpdf",
     ],
-    python_requires=">=3.7"
+    python_requires = ">=3.7"
 
 )
