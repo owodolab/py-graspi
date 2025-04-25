64b333ecba355ffadc7853c5cd0654d53ea3f27d
Julia Joseph <113621027+123julianna@users.noreply.github.com>
2025-04-25 18:30:17 -0400

    descriptor_testing added back to tests directory

--- tests/descriptor_testing.py
+++ tests/descriptor_testing.py
@@ -1,7 +1,7 @@
 import os
 import sys
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../src")))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "../src/py_graspi")))
 import graph as ig
 import descriptors as d
 
