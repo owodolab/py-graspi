72463736196d0989511a4a40ced65aab018502cb
Julia Joseph <113621027+123julianna@users.noreply.github.com>
2025-04-25 14:12:17 -0400

    changed to compute_desc, added image after pip show.

--- tests/descriptor_testing.py
+++ tests/descriptor_testing.py
@@ -8,7 +8,7 @@ import descriptors as d
 
 graph_data = ig.generateGraphAdj(sys.argv[1])
 
-dic = d.descriptors(graph_data, sys.argv[1])
+dic = d.compute_descriptors(graph_data, sys.argv[1])
 
 for key, value in dic.items():
     print(key, value)
