00d23ba1f4a890eb6ec4df51d469a1d1ea6348e6
LaibahA <laibahinfo05@gmail.com>
2025-06-12 15:42:20 -0400

    changed some documentation to reflect changes

--- tools/__init__.py
+++ tools/__init__.py
@@ -1,2 +1,2 @@
 from .tortuosity import tortuosity_histogram, tortuosity
-from .translations import img_to_txt, myceliumTest, plt_to_txt
+from .translations import img_to_txt, img_to_graph, plt_to_txt
